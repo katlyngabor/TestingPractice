4a5baee5dd0c66f4d2c876288a188928d48aca53
Katlyn Gabor <katlyngabor@gmail.com>
2014-07-08 08:52:25 -0400

    gulped

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,20 +1,16 @@
 var plucking = _.pluck([ {type: 'kittens', desc: 'cute'}, {type: 'puppies', desc: 'precious'}], 
  		function(pets){ return pets.type});
 
-	console.log(plucking);
 
 var lasting = _.last([1, 2, 3, 4]);
 
-	console.log(lasting);
 
 var firsting = _.first([1, 2, 3, 4]);
 
-	console.log(firsting);
+
 
 var randoming = _.random([1, 2, 3, 4]);
 	
-	console.log(randoming);
-
 
 
 var Kid = function (options) {
